42d7f7545bb92c74ad04ccdbb85e85a2ede36db4
Aithusa <32020141+Aithosa@users.noreply.github.com>
2018-10-31 09:50:32 +0800

    fix little bug

--- Matlab/multiplex_networks.m
+++ Matlab/multiplex_networks.m
@@ -6,50 +6,50 @@
 % 已知信息S节点感染率衰减sigma_infect
 % I节点在知道信息后的传染率衰减sigma_I
 % S节点在知道信息后的防御系数sigma_S
-% S节点在不知情时接触I节点后知道信息的概率K(是和I节点接触还是分知情与?)-可以暂时不??虑
+% S节点在不知情时接触I节点后知道信息的概率K(是和I节点接触还是分知情与?)-可以暂时不考虑
 % I节点在知道信息后的康复加快率sigma_recover
 
 % ----------加载数据----------
 % clearvars -except A;
 % clear all;
 % load BA_2000_3;	% 加载接触层邻接矩阵A
-% load WS_2000_4_03? % 加载信息层邻接矩阵B
+% load WS_2000_4_03 % 加载信息层邻接矩阵B
 
 % ---------- 公共参数及初始化 ----------
 loop = 10;	% 蒙特卡洛模拟次数
 total_steps = 100;	% 总的时间步数
-N = length(A);	% 网络节点?
-p = round(rand * N);	% ??始随机??出?个感染节点，四舍五入
+N = length(A);	% 网络节点数
+p = round(rand * N);	% 初始始随机选出一个感染节点，四舍五入
 
 % ---------- SIS参数及初始化 ----------
 bata = 0.2;	 % 感染概率
 mu = 0.5;	% 恢复概率
 
-Nodes_SIS = zeros(total_steps, N);   % N行代表时间步数，T列代表节点数，展示每?步所有节点的状??快?
+Nodes_SIS = zeros(total_steps, N);   % N行代表时间步数，T列代表节点数，展示每步所有节点的状态快照
 Nodes_SIS(1, p) = 1;	% 第一步标出初始感染节点的位置
 
-infective_count = zeros(1, total_steps);	% 每步感染点数?,用于?终统?
+infective_count = zeros(1, total_steps);	% 每步感染点数?,用于最终统计
 
 % 每一步两个状态的节点数量
 I = zeros(1, total_steps);	% 多少loop平均后的? 健康群体
 % S = zeros(1, total_steps);	% 多少loop平均后的? 感染群体
 
 % ---------- UAU参数及初始化 ----------
-lambda = 0.3;	% 传播?
-delta = 0.3;	% 遗忘?
+lambda = 0.3;	% 传播率
+delta = 0.3;	% 遗忘率
 
-Nodes_UAU = zeros(total_steps, N);	% 存储网络中每个节点的状??快?
-Nodes_UAU(1, p) = 1;	% 随机初始化一个节点使其处于感染??，当前可感染状态节点的快照
+Nodes_UAU = zeros(total_steps, N);	% 存储网络中每个节点的状态快照
+Nodes_UAU(1, p) = 1;	% 随机初始化一个节点使其处于感染状态，当前可感染状态节点的快照
 
-awareness_count = zeros(1, total_steps);	% 每步知晓节点数量,用于?终统?
+awareness_count = zeros(1, total_steps);	% 每步知晓节点数量,用于最终统计
 
 % ---------- 其他参数 ----------
-aplha = 0.8;	% 信息上传?
+aplha = 0.8;	% 信息上传率
 K = 0.3;	% S节点在不知情时接触I节点后知道信息的概率
-sigma_forget = 0.8;	% 已知信息I节点信息遗忘率衰?
-% sigma_infect = 0.3;	% 已知信息S节点感染率衰?
+sigma_forget = 0.8;	% 已知信息I节点信息遗忘率衰减
+% sigma_infect = 0.3;	% 已知信息S节点感染率衰减
 sigma_I = 0.6;	% I节点在知道信息后的感染率衰减
-sigma_S = 0.3;	% S节点在知道信息后的防御系?
+sigma_S = 0.3;	% S节点在知道信息后的防御系数
 sigma_recover = 1.3;	% I节点在知道信息后的康复加快率
 
 % ---------- 蒙特卡罗次数 ----------
@@ -60,11 +60,12 @@ for circles = 1 : loop
 
 		% ---------- UAU演化 ----------
 
-		% 找到可以传播的活跃节?
+		% 找到可以传播的活跃节点
 		active_node = find((Nodes_UAU(t, :) == 1));	% 找到当前有传播力的节点的位置
 		Num_active_node = length(active_node);	% 统计这一刻有传播力节点的数量
 		infective_source = Num_active_node;	% 当前活跃节点数量
 
+		% 统计当前已知信息节点数
 		awareness_count(t) = awareness_count(t) + sum(Nodes_UAU(t, :));
 
 		if Num_active_node > 0	% 如果还有节点具有传播能力
@@ -105,23 +106,24 @@ for circles = 1 : loop
 		% ---------- SIS演化 ----------
 
 		infective_count(t) = infective_count(t) + sum(Nodes_SIS(t, :));
-		for i = 1 : N	% 考察遍每?个节?
+
+		for i = 1 : N	% 考察遍每个节点
 
 			% ---------- SIS感染过程 ----------
-			if Nodes_SIS(t, i) == 0	% 0代表易感(S)，如果上?步节点i健康，就看他的邻居有没有被感染的
+			if Nodes_SIS(t, i) == 0	% 0代表易感(S)，如果上一步节点i健康，就看他的邻居有没有被感染的
 				
-				neighbor_total = find((A(i, :) == 1));	% 查找其邻?
+				neighbor_total = find((A(i, :) == 1));	% 查找其邻居
 				infective_nodes = find((Nodes_SIS(t, :) == 1));
 				neighbor_infective = setdiff(neighbor_total, infective_nodes);	% 
 				Num_neighbor_infective = length(neighbor_infective);
 
 				% ---------- SIS感染率改变规? ----------
 				if Nodes_UAU(t, i) == 0
-					rate_temp = 1;	% 用于计算感染?
+					rate_temp = 1;	% 用于计算感染率
 
 					for j = 1 : Num_neighbor_infective
 
-						infect_rate_current = bata;	% 当前临时感染?
+						infect_rate_current = bata;	% 当前临时感染率
 
 						if Nodes_UAU(t, neighbor_infective(j)) == 0
 							rate_temp = rate_temp * (1 - infect_rate_current);
@@ -129,7 +131,7 @@ for circles = 1 : loop
 							infect_rate_current = infect_rate_current * sigma_I;
 							rate_temp = rate_temp * (1 - infect_rate_current);
 
-							% ---------- S节点和I节点接触后知? ----------
+							% ---------- S节点和I节点接触后知晓 ----------
 							wake_rate = rand;
 							if wake_rate <= K
 								Nodes_UAU(t+1, i) = 1;
@@ -143,7 +145,7 @@ for circles = 1 : loop
 
 					for j = 1 : Num_neighbor_infective
 
-						infect_rate_current = bata * sigma_S;	% 当前临时感染?
+						infect_rate_current = bata * sigma_S;	% 当前临时感染率
 
 						if Nodes_UAU(t, neighbor_infective(j)) == 0
 							rate_temp = rate_temp * (1 - infect_rate_current);
@@ -190,15 +192,15 @@ end
 infective_count = infective_count ./ loop;
 awareness_count = awareness_count ./ loop;
 
-% ---------- 数据可视? ----------
-t = (1 : 1 : loop);
+% ---------- 数据可视化 ----------
+t = (1 : 1 : total_steps);
 
 % SIS统计
-plot(t,infective_count,'-o','color','y','linewidth',1.2);
+plot(t,infective_count ./ N,'-o','color','y','linewidth',1.2);
 hold on;
 
 % UAU统计
-plot(t, awareness_count, '-o', 'color', 'k', 'linewidth', 1.2);
+plot(t, awareness_count ./ N, '-o', 'color', 'k', 'linewidth', 1.2);
 hold on;
 
 xlabel('steps');ylabel('density of nodes');
