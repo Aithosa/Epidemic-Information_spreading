b98d35172cd80674249d386cab79a11ac394961d
Aithusa <32020141+Aithosa@users.noreply.github.com>
2018-11-28 21:32:58 +0800

    silence time killing code

--- Python/analysis.py
+++ Python/analysis.py
@@ -3,24 +3,28 @@ import numpy as np
 import pandas as pd
 import scipy.io as scio
 
-dataFile = '../Matlab/data/BA_2000_3.mat'  
+dataFile = '../Matlab/data/ER_2000_00075.mat'  
 data = scio.loadmat(dataFile)
-data['A'].shape
+network_scale = data['A'].shape
 
-BA = nx.Graph(data['A'])
-average_shortest_path_length = nx.average_shortest_path_length(BA)
-print('average shortest path length: ', average_shortest_path_length)
+network = nx.Graph(data['A'])
+
+# average_shortest_path_length = nx.average_shortest_path_length(network)
+# print('average shortest path length: ', average_shortest_path_length)
 
 #计算图的平均度
 def average_degree(G):
     dict_vertex_degree = G.degree()    #提取出所有的度
     df_vertex_degree = pd.DataFrame(list(dict(dict_vertex_degree).items()), columns=['vertex','degree'])    #转换为dataframe
+    
     aver_degree = df_vertex_degree['degree'].mean()    #求平均
+
     k_2 = df_vertex_degree['degree']**2
     aver_degree_square = k_2.mean()
+
     return aver_degree, aver_degree_square
 
-degrees = average_degree(BA)
+degrees = average_degree(network)
 print("<k>：",degrees[0])
 
 print("BA: lambd_c = ",degrees[0]/degrees[1])
