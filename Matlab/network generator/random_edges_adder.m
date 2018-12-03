840b8aa9a5044aea549217c2d7face4f013c68ae
Aithusa <32020141+Aithosa@users.noreply.github.com>
2018-12-03 13:59:48 +0800

    Update random_edges_adder.m

--- Matlab/network generator/random_edges_adder.m
+++ Matlab/network generator/random_edges_adder.m
@@ -1,7 +1,9 @@
 load ('../data/BA_2000_3.mat');
 N = length(A);
 
-for i = 1:400
+edges_add = 400;
+
+for i = 1:edges_add
     p1 = round(rand * N);
     p2 = round(rand * N);
     while A(p1, p2) == 1
